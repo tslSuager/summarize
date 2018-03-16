d9766eb21a50ae94c6f3f2c873a733984fc598fc
tsl <2311287492@qq.com>
2018-03-16 17:35:05 +0800

    修改user数据库

--- teach/src/main/java/com/tosit/ssm/mapper/UserMapper.java
+++ teach/src/main/java/com/tosit/ssm/mapper/UserMapper.java
@@ -4,9 +4,27 @@ import com.tosit.ssm.entity.User;
 import com.tosit.ssm.entity.UserExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserMapper {
+    long countByExample(UserExample example);
 
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
 }
