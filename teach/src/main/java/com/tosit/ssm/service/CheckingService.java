19aa0ac0dfb1fbeb48abe0ec890dd1ca330b4476
tsl <2311287492@qq.com>
2018-03-19 10:12:19 +0800

    mapper完善

--- teach/src/main/java/com/tosit/ssm/service/CheckingService.java
+++ teach/src/main/java/com/tosit/ssm/service/CheckingService.java
@@ -3,5 +3,5 @@ package com.tosit.ssm.service;
 import com.tosit.ssm.entity.KaoqinRule;
 
 public interface CheckingService {
-    public int addKaoQinRule(KaoqinRule kaoqinRule);
+    int addKaoQinRule(KaoqinRule kaoqinRule);
 }
