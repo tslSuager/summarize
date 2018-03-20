2c9ee5087f4c2ee14be88a49049e645c10ab7848
tsl <2311287492@qq.com>
2018-03-20 15:49:47 +0800

    区域下拉插件

--- teach/src/main/java/com/tosit/ssm/service/OfficeServiceImpl.java
+++ teach/src/main/java/com/tosit/ssm/service/OfficeServiceImpl.java
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class OfficeServiceImpl {
+public class OfficeServiceImpl implements OfficeService{
     @Autowired
     OfficeMapper officeMapper;
 
     public void createOffice(Office office) {
         officeMapper.insertOffice(office);
     }
+
 
     public List<Office> getAllArea() {
         return  officeMapper.selectAllArea();
