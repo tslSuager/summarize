d2c5f3abc5d32845d4537d250b2e593d8bfc759a
hanqi <1451550958@qq.com>
2018-03-21 16:44:33 +0800

    计划相关表的增加 删除 修改的sql语句

--- teach/src/main/java/com/tosit/ssm/mapper/TaskResultMapper.java
+++ teach/src/main/java/com/tosit/ssm/mapper/TaskResultMapper.java
@@ -7,24 +7,28 @@ import org.apache.ibatis.annotations.Param;
 
 public interface TaskResultMapper {
     long countByExample(TaskResultExample example);
-
     int deleteByExample(TaskResultExample example);
-
     int deleteByPrimaryKey(String id);
-
-    int insert(TaskResult record);
-
     int insertSelective(TaskResult record);
-
     List<TaskResult> selectByExample(TaskResultExample example);
-
     TaskResult selectByPrimaryKey(String id);
-
     int updateByExampleSelective(@Param("record") TaskResult record, @Param("example") TaskResultExample example);
-
     int updateByExample(@Param("record") TaskResult record, @Param("example") TaskResultExample example);
-
     int updateByPrimaryKeySelective(TaskResult record);
 
+
+
+    /**
+     * 插入一个任务结果表
+     * @param record 一个包含id的任务结果对象
+     * @return
+     */
+    int insertTaskResult(TaskResult record);
+
+    /**
+     * 更新一个任务结果表
+     * @param record 一个包含id的任务结果对象
+     * @return
+     */
     int updateByPrimaryKey(TaskResult record);
 }
