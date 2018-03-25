19d5f564e88df9ef05455dc5fdb6ed4ccb490fb1
tsl <2311287492@qq.com>
2018-03-25 17:51:46 +0800

    Merge branch 'master' of https://github.com/tslSuager/summarize
    # Conflicts:
    #	teach/src/main/java/com/tosit/ssm/controller/CheckingController.java
    #	teach/src/main/java/com/tosit/ssm/mapper/UserMapper.xml

--- teach/src/main/java/com/tosit/ssm/controller/CheckingController.java
+++ teach/src/main/java/com/tosit/ssm/controller/CheckingController.java
@@ -7,7 +7,9 @@ import com.tosit.ssm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.Time;
